bb9f28410ea2b59f699f26886ff5d2a5732b2338
MACBOOK AIR <macbookair@MACBOOK.local>
2023-12-16 17:27:56 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:first_app/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 70, 33, 133),
+                Color.fromARGB(255, 82, 43, 148),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
---
+++ test/test.md
@@ -0,0 +1,13 @@
+---
+runme:
+  id: 01HG89GSW21H2MMQGNVR2BT0DG
+  version: v2.0
+---
+
+Hi new changes
+
+```sh {"id":"01HGB8FTAY1H9HAW59BEKQQE9N"}
+echo "hello world"
+```
+
+hhjh
